951b88dc4262bc9894c23fc16f6b7d055680f76d
Samuel Farias <samuellima810@gmail.com>
2019-08-05 15:41:45 -0300

    importação com problemas

--- CAP-08/Funcoes.py
+++ CAP-08/Funcoes.py
@@ -53,29 +53,73 @@
 
 #############  Passando uma lista para uma função  ##################
 
-nomesPessoas = ['samuel','lima','farias','teste']
+# nomesPessoas = ['samuel','lima','farias','teste']
+#
+# def saudarPessoas(listaPessoas):
+#     for pessoa in listaPessoas:
+#         print("Bem-vindo " + pessoa.title())
+#
+#         #### Modificando a lista ###
+#         if "maria" not in listaPessoas:
+#             listaPessoas.append("maria")
+#
+#
+# saudarPessoas(nomesPessoas)
+# print("###########################################")
+# nomesPessoasCopia = nomesPessoas[:]
+# saudarPessoas(nomesPessoasCopia)
+#
+# print("###########################################")
+#
+# ############### Passando um número arbitrário de argumentos ##########################
+# print("############### Passando um número arbitrário de argumentos ##########################")
+#
+# def pizza(*sabores):  ##o (*) faz com que o python crie uma tupla vazia e seja preenchida com o argumentos
+#     print("PIZZA :")
+#     for sabor in sabores:
+#         print(sabor)
+#
+# pizza('calabresa','cebola','queijo')
+# pizza('calabresa')
+# pizza('calabresa','tomate')
+#
+# print("############### Passando um número arbitrário de argumentos ##########################")
 
-def saudarPessoas(listaPessoas):
-    for pessoa in listaPessoas:
-        print("Bem-vindo " + pessoa.title())
+#############  Misturando argumentos posicionais e arbitrários  ############################
 
-        #### Modificando a lista ###
-        if "maria" not in listaPessoas:
-            listaPessoas.append("maria")
+######## OBS.:    Se quiser que uma função aceite vários tipos de argumentos, o
+                # parâmetro que aceita um número arbitrário de argumentos deve ser
+                # colocado por último na definição da função.  Ou seja, o argumento com ( * ) deve ser colocado no final
+
+# def pizza(tamanho,*sabores):
+#     print("Pizza tamanho: " + str(tamanho))
+#     for sabor in sabores:
+#         print(sabor)
+#
+#
+# pizza(2,'calabresa', 'cebola', 'queijo')
+# pizza(3,'calabresa')
+# pizza(4,'calabresa', 'tomate')
+##################################################################################################
+
+# def perfil(nome, sobreNome, **informacoes):  ## os asteriscos duplos ( ** ) faz com que o python crie um dicionario vazio para aceitar as informações
+#     perfilUsuario = {}
+#     perfilUsuario['nome'] = nome
+#     perfilUsuario['sobreNome'] = sobreNome
+#
+#     for chave, valor in informacoes.items():
+#         perfilUsuario[chave] = valor
+#
+#     return perfilUsuario
+#
+#
+# perfil_usuario = perfil('samuel','farias',location = 'salvador',cargo = 'T.I')
+#
+# print(perfil_usuario)
 
 
-saudarPessoas(nomesPessoas)
-print("###########################################")
-nomesPessoasCopia = nomesPessoas[:]
-saudarPessoas(nomesPessoasCopia)
-
-print("###########################################")
-
-
-
-
-
-
+############################ Armazenando suas funções em módulos ############################
+############################ Importando um módulo completo  ##############################
 
 
 
---
+++ CAP-08/funcaoPizza.py
@@ -0,0 +1,9 @@
+##### ESSA FUNÇÃO SERÁ IMPORTADA POR OUTRO ARQUIVO - FUNCOES.PY
+
+def pizza(tamanho, *ingredientes):
+    print("Tamanho da PIZZA: " + str(tamanho))
+    for ingrediente in ingredientes:
+        print(ingrediente)
+
+
+##############################################################33
